7a8b18e4ae5142545672cdc713fbd796420d65eb
Shmarte.1970 <shmarte@gmail.com>
2022-05-20 17:41:53 +0200

    quiz v2.5

--- data/data.js
+++ data/data.js
@@ -138,7 +138,7 @@ export const data = [
         choise:[
             'did',
             'does',
-            'do'
+            'doed'
         ],
         answer: 'did'
 
@@ -164,7 +164,117 @@ export const data = [
         ],
         answer: 'read'
 
+    },
+    {
+        question:'M E E T',
+        choise:[
+            'meet',
+            'met',
+            'meant'
+        ],
+        answer: 'met'
+
+    },
+    {
+        question:'W I N',
+        choise:[
+            'won',
+            'win',
+            'worn'
+        ],
+        answer: 'won'
+
+    },
+     {
+        question:'P L A Y E D',
+        choise:[
+            'play',
+            'played',
+            'playing'
+        ],
+        answer: 'played'
+
+    },
+    {
+        question:'B U Y',
+        choise:[
+            'bought',
+            'buy',
+            'bougt'
+        ],
+        answer: 'bought'
+
+    },
+    {
+        question:'C A T C H',
+        choise:[
+            'catching',
+            'caught',
+            'cat'
+        ],
+        answer: 'caught'
+
+    },
+    {
+        question:'J O I N S',
+        choise:[
+            'join',
+            'joined',
+            'joining'
+        ],
+        answer: 'joined'
+
+    },
+    {
+        question:'L I V E',
+        choise:[
+            'living',
+            'lived',
+            'lives'
+        ],
+        answer: 'lived'
+
+    }, 
+    {
+        question:'A R R I V E',
+        choise:[
+            'arrived',
+            'to arrive'
+        ],
+        answer: 'arrived'
+
+    },
+    {
+        question:'rain',
+        choise:[
+            'rained',
+            'to rain'
+            
+        ],
+        answer: 'rained'
+
+    },
+    {
+        question:'walks',
+        choise:[
+            'walked',
+            'walking'
+            
+        ],
+        answer: 'walked'
+
+    },
+    {
+        question:'G I V E S',
+        choise:[
+            'gave',
+            'giving',
+            'given'
+        ],
+        answer: 'gave'
+
     }
+
 
 
 
